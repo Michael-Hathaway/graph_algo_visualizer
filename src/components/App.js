be7597454f3900ae9ec49f4dfab00c6066e13e1e
Michael Hathaway <mikehath5@gmail.com>
2020-11-04 11:21:03 -0800

    Set up project and added started files.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../style/App.css";
+
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div className="page-wrapper">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
